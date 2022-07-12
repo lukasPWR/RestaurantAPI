c0c75f68e8eb9b8254a0a8994a46c0c72125fb0b
lukasPWR <jnowicki91@gmail.com>
2022-07-12 14:35:18 +0200

    Add project files.

---
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly IRestaurantService _restaurantService;
+        public RestaurantController(IRestaurantService restaurantService)
+        {
+            _restaurantService = restaurantService;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update([FromBody] UpdateRestaurantDto dto, [FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var isUpdated = _restaurantService.Update(dto, id);
+            if (!isUpdated)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+           var isDeleted = _restaurantService.Delete(id);
+
+           if (isDeleted)
+           {
+               return NoContent();
+           }
+
+           return NotFound();
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
+        {
+            var restaurantsDtos = _restaurantService.GetAll();
+
+            return Ok(restaurantsDtos);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<RestaurantDto> Get([FromRoute] int id)
+        {
+            var restaurant = _restaurantService.GetById(id);
+            if (restaurant is null)
+            {
+                return NotFound();
+            }
+
+           
+            return Ok(restaurant);
+        }
+
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+           var id = _restaurantService.Create(dto);
+            
+
+            return Created($"/api/restaurant/{id}", null);
+        }
+
+       
+       
+
+    }
+}
---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestaurantAPI.Entities;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext  dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description =
+                        "KFC (short for Kentucky Fried Chhicken) is an American fast food restaurant chain headquartered",
+                    ContactEmail = "contact@kfc.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Price = 10.30M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggets",
+                            Price = 5.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Krakow",
+                        Street = "Dluga 5",
+                        PostalCode = "30-001"
+                    }
+
+                },
+                new Restaurant()
+                {
+                    Name = "McDonald",
+                    Category = "Fast Food",
+                    Description = "Dobre jedzonko",
+                    ContactEmail = "contact2@mcdonald.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Cheese Burger",
+                            Price = 5.30M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Big Mac",
+                            Price = 10.50M,
+                        },
+
+                    },
+                    Address = new Address()
+                    {
+                        City = "Krakow",
+                        Street = "Krotka 6",
+                        PostalCode = "30-001"
+                    }
+                }
+            };
+            return restaurants;
+        }
+    }
+}
